9772a822e12f7f7ec6f10b6230a6f1a8ce3070cc
Maxito7 <guyhozanto@gmail.com>
2023-12-23 08:28:53 -0500

    zsh extras

--- home-manager/configs/shells/zsh.nix
+++ home-manager/configs/shells/zsh.nix
@@ -1,9 +1,20 @@
 {
 	programs.zsh = {
 		enable = true;
+		syntaxHighlighting.enavle = true;
+		enableAutosuggestions = true;
+		history.path = "/home/lucky/.config/zsh/zsh_history";
 		shellAliases = {
 			flake_update = "nix flake update";
 			flake_switch = "sudo nixos-rebuild switch --flake .#lucky";
+			giti = "git init";
+			gita = "git add .";
+			gitc = "git commit -m";
+			gitp = "git push";
+			gitrao = "git remote add origin";
+			gitbm = "git branch -M";
+			gitpuo = "git push -u origin";
+			gitrso = "git remote set-url origin";
 		};
 		#histSize = 10000;
 		#histFile = "~/.config/nixfiles/home-manager/configs/shells/zsh/history";
