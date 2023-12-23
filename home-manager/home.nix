502391ca502797e86b902d7d6be0a79965f1283a
Maxito7 <guyhozanto@gmail.com>
2023-12-23 12:23:08 -0500

    Linking Zoxide with Fish IV

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -107,5 +107,9 @@
   home.homeDirectory = "/home/lucky";
   home.stateVersion = "22.05";
 	
-  programs.home-manager.enable = true;	
+  programs.home-manager.enable = true;
+	programs.zoxide = {
+		enable = true;
+		enableFishIntegration = true;
+	};
 }
