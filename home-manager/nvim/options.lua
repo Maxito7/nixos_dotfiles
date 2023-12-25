d0b5b22bed90981638f203ff3d33a629740797f2
Maxito7 <guyhozanto@gmail.com>
2023-12-25 08:23:47 -0500

    Fixing Neovim (again ffs) fuucccck VI

--- home-manager/nvim/options.lua
+++ home-manager/nvim/options.lua
@@ -163,74 +163,6 @@ require("lazy").setup({
 ]]
 
 -- DEL. THE COMMENTED CODE HERE IF NTH. WORKS! I DON'T KNOW WHAT THE FUCK I'M DOING! :D
--- Web-Devicons
-require("nvim-web-devicons").setup({
-	default = false,
-	override = {
-		rs = {
-			icon = "",
-			color = "#ff9e64",
-			name = "Rust",
-		},
-		lua = {
-			icon = "󰢱",
-			color = "#7DCFFF",
-			name = "Lua",
-		},
-		py = {
-			icon = "",
-			color = "#7bd88f",
-			name = "Python",
-		},
-		go = {
-			icon = "",
-			color = "#7dcfff",
-			name = "Go",
-		},
-		cpp = {
-			icon = "",
-			color = "#7aa2f7",
-			name = "CPP",
-		},
-		c = {
-			icon = "",
-			color = "#BB9AF7",
-			name = "C",
-		},
-		tex = {
-			icon = "",
-			color = "#9AA5CE",
-			name = "LaTeX",
-		},
-		vim = {
-			icon = "",
-			color = "#7BD88F",
-			name = "Vim",
-		},
-		vue = {
-			icon = "",
-			color = "#7bd88f",
-			name = "Vue",
-		},
-	},
-	override_by_extension = {
-		["norg"] = {
-			icon = "",
-			color = "#4878BE",
-			name = "Neorg",
-		},
-		["py"] = {
-			icon = "",
-			color = "#7bd88f",
-			name = "Python",
-		},
-	},
-	-- globally enable different highlight colors per icon (default to true)
-	-- if set to false all icons will have the default icon's color
-	color_icons = true,
-})
-
-
 -- Lualine
 require("lualine").setup({
 	icons_enabled = true,
@@ -902,4 +834,71 @@ require("nvim-treesitter.configs").setup({
 			})
 		end,
 	},
+})
+
+-- Web-Devicons
+require("nvim-web-devicons").setup({
+	default = false,
+	override = {
+		rs = {
+			icon = "",
+			color = "#ff9e64",
+			name = "Rust",
+		},
+		lua = {
+			icon = "󰢱",
+			color = "#7DCFFF",
+			name = "Lua",
+		},
+		py = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Python",
+		},
+		go = {
+			icon = "",
+			color = "#7dcfff",
+			name = "Go",
+		},
+		cpp = {
+			icon = "",
+			color = "#7aa2f7",
+			name = "CPP",
+		},
+		c = {
+			icon = "",
+			color = "#BB9AF7",
+			name = "C",
+		},
+		tex = {
+			icon = "",
+			color = "#9AA5CE",
+			name = "LaTeX",
+		},
+		vim = {
+			icon = "",
+			color = "#7BD88F",
+			name = "Vim",
+		},
+		vue = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Vue",
+		},
+	},
+	override_by_extension = {
+		["norg"] = {
+			icon = "",
+			color = "#4878BE",
+			name = "Neorg",
+		},
+		["py"] = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Python",
+		},
+	},
+	-- globally enable different highlight colors per icon (default to true)
+	-- if set to false all icons will have the default icon's color
+	color_icons = true,
 })
