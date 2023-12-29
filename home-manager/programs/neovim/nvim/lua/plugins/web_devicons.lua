12cd76664a0aadd63ebb5a77e340ee7aa37e7448
Maxito7 <guyhozanto@gmail.com>
2023-12-29 14:33:03 -0500

    Another try at web-devicons #9

--- home-manager/programs/neovim/nvim/lua/plugins/web_devicons.lua
+++ home-manager/programs/neovim/nvim/lua/plugins/web_devicons.lua
@@ -3,72 +3,70 @@ return {
 	lazy = false,
 	--event = "VeryLazy",
 	priority = 2000,
-	--[[
-	config = function()
-		-- Web-Devicons
-		require("nvim-web-devicons").setup({
-			default = true,
-			color_icons = true,
-			default_icon = {
-				icon = "",
-				name = "Default",
+	opts = {
+		default = true,
+		color_icons = true,
+		default_icon = {
+			icon = "",
+			name = "Default",
+		},
+		override = {
+			rs = {
+				icon = " ",
+				color = "#ff9e64",
+				name = "Rust",
 			},
-			override = {
-				rs = {
-					icon = " ",
-					color = "#ff9e64",
-					name = "Rust",
-				},
-				lua = {
-					icon = "󰢱",
-					color = "#7DCFFF",
-					name = "Lua",
-				},
-				py = {
-					icon = "",
-					color = "#7bd88f",
-					name = "Python",
-				},
-				go = {
-					icon = "",
-					color = "#7dcfff",
-					name = "Go",
-				},
-				cpp = {
-					icon = "",
-					color = "#7aa2f7",
-					name = "CPP",
-				},
-				c = {
-					icon = "",
-					color = "#BB9AF7",
-					name = "C",
-				},
-				tex = {
-					icon = "",
-					color = "#9AA5CE",
-					name = "LaTeX",
-				},
-				vim = {
-					icon = "",
-					color = "#7BD88F",
-					name = "Vim",
-				},
-				vue = {
-					icon = "",
-					color = "#7bd88f",
-					name = "Vue",
-				},
+			lua = {
+				icon = "󰢱",
+				color = "#7DCFFF",
+				name = "Lua",
 			},
-			override_by_extension = {
-				["norg"] = {
-					icon = "",
-					--color = "#4878BE",
-					color = "#7AA2F7",
-					name = "Neorg",
-				},
+			py = {
+				icon = "",
+				color = "#7bd88f",
+				name = "Python",
 			},
-		})
+			go = {
+				icon = "",
+				color = "#7dcfff",
+				name = "Go",
+			},
+			cpp = {
+				icon = "",
+				color = "#7aa2f7",
+				name = "CPP",
+			},
+			c = {
+				icon = "",
+				color = "#BB9AF7",
+				name = "C",
+			},
+			tex = {
+				icon = "",
+				color = "#9AA5CE",
+				name = "LaTeX",
+			},
+			vim = {
+				icon = "",
+				color = "#7BD88F",
+				name = "Vim",
+			},
+			vue = {
+				icon = "",
+				color = "#7bd88f",
+				name = "Vue",
+			},
+		},
+		override_by_extension = {
+			["norg"] = {
+				icon = "",
+				--color = "#4878BE",
+				color = "#7AA2F7",
+				name = "Neorg",
+			},
+		},
+	},
+	config = function(_, opts) --(_, opts)
+		require("nvim-web-devicon").setup(opts)
 	end,
-	]]
 }
