3ebdd7768211488cad98d325bb78899a6db31a27
Maxito7 <guyhozanto@gmail.com>
2023-12-29 15:24:10 -0500

    Another try at web-devicons v2 #4

--- home-manager/programs/neovim/nvim/lua/core/init.lua
+++ home-manager/programs/neovim/nvim/lua/core/init.lua
@@ -2,6 +2,13 @@ require("core.options")
 require("core.lazy")
 require("core.ui")
 require("core.keymaps")
+require("nvim-web-devicons").set_icon({
+	lua = {
+		icon = "󰢱",
+		color = "#7DCFFF",
+		name = "Lua",
+	},
+})
 local icon, color = require("nvim-web-devicons").get_icon_color("init.lua", "lua")
 assert(icon == "󰢱")
 assert(color == "#7DCFFF")
