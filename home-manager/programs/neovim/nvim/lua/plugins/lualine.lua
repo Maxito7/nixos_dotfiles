566b1640bcb123bb53bd7aa7e92ad5fa5c8f2069
Maxito7 <guyhozanto@gmail.com>
2023-12-29 01:09:41 -0500

    Toying with Lualine/web-devicons #17

--- home-manager/programs/neovim/nvim/lua/plugins/lualine.lua
+++ home-manager/programs/neovim/nvim/lua/plugins/lualine.lua
@@ -11,6 +11,69 @@ return {
 	},
 	config = function(_, opts)
 		require("lualine").setup(opts)
+		require("nvim-web-devicons").setup({
+			default = false,
+			color_icons = true,
+			default_icon = {
+				icon = "",
+				name = "Default",
+			},
+			override = {
+				rs = {
+					icon = " ",
+					color = "#ff9e64",
+					name = "Rust",
+				},
+				lua = {
+					icon = "󰢱",
+					color = "#7DCFFF",
+					name = "Lua",
+				},
+				py = {
+					icon = "",
+					color = "#7bd88f",
+					name = "Python",
+				},
+				go = {
+					icon = "",
+					color = "#7dcfff",
+					name = "Go",
+				},
+				cpp = {
+					icon = "",
+					color = "#7aa2f7",
+					name = "CPP",
+				},
+				c = {
+					icon = "",
+					color = "#BB9AF7",
+					name = "C",
+				},
+				tex = {
+					icon = "",
+					color = "#9AA5CE",
+					name = "LaTeX",
+				},
+				vim = {
+					icon = "",
+					color = "#7BD88F",
+					name = "Vim",
+				},
+				vue = {
+					icon = "",
+					color = "#7bd88f",
+					name = "Vue",
+				},
+			},
+			strict = true,
+			override_by_extension = {
+				["norg"] = {
+					icon = "",
+					color = "#4878BE",
+					name = "Neorg",
+				},
+			},
+		})
 	end,
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
