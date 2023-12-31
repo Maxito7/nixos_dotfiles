1387bf22fc8847e87836d4897d74afaacc05dd55
Maxito7 <guyhozanto@gmail.com>
2023-12-31 14:29:07 -0500

    Setting up plugins with Balls #32

--- home-manager/programs/neovim/nvim/lua/core/balls.lua
+++ home-manager/programs/neovim/nvim/lua/core/balls.lua
@@ -130,6 +130,77 @@ end
                                                           \|_________|
 ]]
 
+-- Devicons
+local devicons = require("nvim-web-devicons")
+
+devicons.setup({
+	default = false,
+	color_icons = true,
+	overrides = {
+		lua = {
+			icon = "P",
+			color = "#7DCFFF",
+			name = "Lua",
+		},
+		--[[
+		rs = {
+			icon = " ",
+			color = "#ff9e64",
+			name = "Rust",
+		},
+		py = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Python",
+		},
+		go = {
+			icon = "",
+			color = "#7dcfff",
+			name = "Go",
+		},
+		cpp = {
+			icon = "",
+			color = "#7aa2f7",
+			name = "CPP",
+		},
+		c = {
+			icon = "",
+			color = "#BB9AF7",
+			name = "C",
+		},
+		tex = {
+			icon = "",
+			color = "#9AA5CE",
+			name = "LaTeX",
+		},
+		vim = {
+			icon = "",
+			color = "#7BD88F",
+			name = "Vim",
+		},
+		vue = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Vue",
+		},
+		]]
+	},
+	--[[
+	default_icon = {
+		icon = "",
+		name = "Default",
+	},
+	override_by_extension = {
+		["norg"] = {
+			icon = "",
+			--color = "#4878BE",
+			color = "#7AA2F7",
+			name = "Neorg",
+		},
+	},
+	]]
+})
+
 -- Tokyonight
 local tokyonight = require("tokyonight")
 tokyonight.setup({
@@ -786,72 +857,5 @@ require("mini.animate").setup({
 	},
 	close = {
 		enable = false,
-	},
-})
-
--- Devicons
-local devicons = require("nvim-web-devicons")
-
-devicons.setup({
-	default = false,
-	color_icons = true,
-	overrides = {
-		rs = {
-			icon = " ",
-			color = "#ff9e64",
-			name = "Rust",
-		},
-		lua = {
-			icon = "󰢱",
-			color = "#7DCFFF",
-			name = "Lua",
-		},
-		py = {
-			icon = "",
-			color = "#7bd88f",
-			name = "Python",
-		},
-		go = {
-			icon = "",
-			color = "#7dcfff",
-			name = "Go",
-		},
-		cpp = {
-			icon = "",
-			color = "#7aa2f7",
-			name = "CPP",
-		},
-		c = {
-			icon = "",
-			color = "#BB9AF7",
-			name = "C",
-		},
-		tex = {
-			icon = "",
-			color = "#9AA5CE",
-			name = "LaTeX",
-		},
-		vim = {
-			icon = "",
-			color = "#7BD88F",
-			name = "Vim",
-		},
-		vue = {
-			icon = "",
-			color = "#7bd88f",
-			name = "Vue",
-		},
-	},
-	default_icon = {
-		icon = "",
-		name = "Default",
-	},
-	override_by_extension = {
-		["norg"] = {
-			icon = "",
-			--color = "#4878BE",
-			color = "#7AA2F7",
-			name = "Neorg",
-		},
 	},
 })
