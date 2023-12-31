c2f88110d20d9f659dfead08335c3de81218db62
Maxito7 <guyhozanto@gmail.com>
2023-12-31 14:18:35 -0500

    Setting up plugins with Balls #28

--- home-manager/programs/neovim/nvim/lua/core/balls.lua
+++ home-manager/programs/neovim/nvim/lua/core/balls.lua
@@ -2,24 +2,28 @@ local config_path = vim.fn.stdpath("config")
 local balls_path = vim.fs.joinpath(config_path, "pack", "balls", "start", "balls.nvim")
 
 if vim.uv.fs_stat(balls_path) == nil then
-  local command = {
-    "git",
-    "clone",
-    "--depth",
-    "1",
-    "https://github.com/TheBallsUp/balls.nvim",
-    balls_path,
-  }
+	local command = {
+		"git",
+		"clone",
+		"--depth",
+		"1",
+		"https://github.com/TheBallsUp/balls.nvim",
+		balls_path,
+	}
 
-  vim.system(command, {}, vim.schedule_wrap(function(result)
-    if result.code ~= 0 then
-      error("Failed to install balls.nvim: " .. result.stderr)
-    end
+	vim.system(
+		command,
+		{},
+		vim.schedule_wrap(function(result)
+			if result.code ~= 0 then
+				error("Failed to install balls.nvim: " .. result.stderr)
+			end
 
-    vim.notify("Installed balls.nvim!")
-    vim.cmd.packloadall()
-    vim.cmd.helptags(vim.fs.joinpath(balls_path, "doc"))
-  end))
+			vim.notify("Installed balls.nvim!")
+			vim.cmd.packloadall()
+			vim.cmd.helptags(vim.fs.joinpath(balls_path, "doc"))
+		end)
+	)
 end
 
 local Balls = require("balls")
@@ -155,73 +159,6 @@ tokyonight.setup({
 
 vim.cmd("colorscheme tokyonight")
 
--- Devicons
-local devicons = require("nvim-web-devicons")
-
-devicons.setup({
-	default = false,
-		color_icons = true,
-		override = {
-			rs = {
-				icon = " ",
-				color = "#ff9e64",
-				name = "Rust",
-			},
-			lua = {
-				icon = "󰢱",
-				color = "#7DCFFF",
-				name = "Lua",
-			},
-			py = {
-				icon = "",
-				color = "#7bd88f",
-				name = "Python",
-			},
-			go = {
-				icon = "",
-				color = "#7dcfff",
-				name = "Go",
-			},
-			cpp = {
-				icon = "",
-				color = "#7aa2f7",
-				name = "CPP",
-			},
-			c = {
-				icon = "",
-				color = "#BB9AF7",
-				name = "C",
-			},
-			tex = {
-				icon = "",
-				color = "#9AA5CE",
-				name = "LaTeX",
-			},
-			vim = {
-				icon = "",
-				color = "#7BD88F",
-				name = "Vim",
-			},
-			vue = {
-				icon = "",
-				color = "#7bd88f",
-				name = "Vue",
-			},
-		},
-		default_icon = {
-			icon = "",
-			name = "Default",
-		},
-		override_by_extension = {
-			["norg"] = {
-				icon = "",
-				--color = "#4878BE",
-				color = "#7AA2F7",
-				name = "Neorg",
-			},
-		},
-})
-
 -- Lualine
 vim.cmd.packadd("lualine.nvim")
 local lualine = require("lualine")
@@ -283,100 +220,100 @@ barbecue.setup({})
 vim.cmd.packadd("oil.nvim")
 local oil = require("oil")
 oil.setup({
-		default_file_explorer = true,
-		columns = {
-			"icon",
-			--"permissions",
-			--"size",
-			--"mtime",
+	default_file_explorer = true,
+	columns = {
+		"icon",
+		--"permissions",
+		--"size",
+		--"mtime",
+	},
+	buf_options = {
+		buflisted = false,
+		bufhidden = "hide",
+	},
+	win_options = {
+		wrap = false,
+		signcolumn = "no",
+		cursorcolumn = false,
+		foldcolumn = "0",
+		spell = false,
+		list = false,
+		conceallevel = 3,
+		concealcursor = "nvic",
+	},
+	delete_to_trash = false,
+	skip_confirm_for_simple_edits = false,
+	trash_command = "trash-put",
+	prompt_save_on_select_new_entry = true,
+	cleanup_delay_ms = 2000,
+	keymaps = {
+		["g?"] = "actions.show_help",
+		["<CR>"] = "actions.select",
+		["<C-s>"] = "actions.select_vsplit",
+		["<C-h>"] = "actions.select_split",
+		["<C-t>"] = "actions.select_tab",
+		["<C-p>"] = "actions.preview",
+		["<C-c>"] = "actions.close",
+		["<C-l>"] = "actions.refresh",
+		["-"] = "actions.parent",
+		["_"] = "actions.open_cwd",
+		["`"] = "actions.cd",
+		["~"] = "actions.tcd",
+		["gs"] = "actions.change_sort",
+		["gx"] = "actions.open_external",
+		["g."] = "actions.toggle_hidden",
+	},
+	use_default_keymaps = true,
+	view_options = {
+		show_hidden = true,
+		is_hidden_file = function(name, bufnr)
+			return vim.startswith(name, ".")
+		end,
+		is_always_hidden = function(name, bufnr)
+			return false
+		end,
+		sort = {
+			{ "type", "asc" },
+			{ "name", "asc" },
 		},
-		buf_options = {
-			buflisted = false,
-			bufhidden = "hide",
-		},
+	},
+	float = {
+		padding = 2,
+		max_width = 0,
+		max_height = 0,
+		border = "rounded",
 		win_options = {
-			wrap = false,
-			signcolumn = "no",
-			cursorcolumn = false,
-			foldcolumn = "0",
-			spell = false,
-			list = false,
-			conceallevel = 3,
-			concealcursor = "nvic",
+			winblend = 0,
 		},
-		delete_to_trash = false,
-		skip_confirm_for_simple_edits = false,
-		trash_command = "trash-put",
-		prompt_save_on_select_new_entry = true,
-		cleanup_delay_ms = 2000,
-		keymaps = {
-			["g?"] = "actions.show_help",
-			["<CR>"] = "actions.select",
-			["<C-s>"] = "actions.select_vsplit",
-			["<C-h>"] = "actions.select_split",
-			["<C-t>"] = "actions.select_tab",
-			["<C-p>"] = "actions.preview",
-			["<C-c>"] = "actions.close",
-			["<C-l>"] = "actions.refresh",
-			["-"] = "actions.parent",
-			["_"] = "actions.open_cwd",
-			["`"] = "actions.cd",
-			["~"] = "actions.tcd",
-			["gs"] = "actions.change_sort",
-			["gx"] = "actions.open_external",
-			["g."] = "actions.toggle_hidden",
+		override = function(conf)
+			return conf
+		end,
+	},
+	preview = {
+		max_width = 0.9,
+		min_width = { 40, 0.4 },
+		width = nil,
+		max_height = 0.9,
+		min_height = { 5, 0.1 },
+		height = nil,
+		border = "rounded",
+		win_options = {
+			winblend = 0,
 		},
-		use_default_keymaps = true,
-		view_options = {
-			show_hidden = true,
-			is_hidden_file = function(name, bufnr)
-				return vim.startswith(name, ".")
-			end,
-			is_always_hidden = function(name, bufnr)
-				return false
-			end,
-			sort = {
-				{ "type", "asc" },
-				{ "name", "asc" },
-			},
+	},
+	progress = {
+		max_width = 0.9,
+		min_width = { 40, 0.4 },
+		width = nil,
+		max_height = { 10, 0.9 },
+		min_height = { 5, 0.1 },
+		height = nil,
+		border = "rounded",
+		minimized_border = "none",
+		win_options = {
+			winblend = 0,
 		},
-		float = {
-			padding = 2,
-			max_width = 0,
-			max_height = 0,
-			border = "rounded",
-			win_options = {
-				winblend = 0,
-			},
-			override = function(conf)
-				return conf
-			end,
-		},
-		preview = {
-			max_width = 0.9,
-			min_width = { 40, 0.4 },
-			width = nil,
-			max_height = 0.9,
-			min_height = { 5, 0.1 },
-			height = nil,
-			border = "rounded",
-			win_options = {
-				winblend = 0,
-			},
-		},
-		progress = {
-			max_width = 0.9,
-			min_width = { 40, 0.4 },
-			width = nil,
-			max_height = { 10, 0.9 },
-			min_height = { 5, 0.1 },
-			height = nil,
-			border = "rounded",
-			minimized_border = "none",
-			win_options = {
-				winblend = 0,
-			},
-		},
+	},
 })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
@@ -858,5 +795,72 @@ require("mini.animate").setup({
 	},
 	close = {
 		enable = false,
+	},
+})
+
+-- Devicons
+local devicons = require("nvim-web-devicons")
+
+devicons.setup({
+	default = false,
+	color_icons = true,
+	override = {
+		rs = {
+			icon = " ",
+			color = "#ff9e64",
+			name = "Rust",
+		},
+		lua = {
+			icon = "󰢱",
+			color = "#7DCFFF",
+			name = "Lua",
+		},
+		py = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Python",
+		},
+		go = {
+			icon = "",
+			color = "#7dcfff",
+			name = "Go",
+		},
+		cpp = {
+			icon = "",
+			color = "#7aa2f7",
+			name = "CPP",
+		},
+		c = {
+			icon = "",
+			color = "#BB9AF7",
+			name = "C",
+		},
+		tex = {
+			icon = "",
+			color = "#9AA5CE",
+			name = "LaTeX",
+		},
+		vim = {
+			icon = "",
+			color = "#7BD88F",
+			name = "Vim",
+		},
+		vue = {
+			icon = "",
+			color = "#7bd88f",
+			name = "Vue",
+		},
+	},
+	default_icon = {
+		icon = "",
+		name = "Default",
+	},
+	override_by_extension = {
+		["norg"] = {
+			icon = "",
+			--color = "#4878BE",
+			color = "#7AA2F7",
+			name = "Neorg",
+		},
 	},
 })
