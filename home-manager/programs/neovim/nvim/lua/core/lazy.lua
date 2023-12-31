b6ea78dfbfad9ccf4a55173b205710943fbd1acc
Maxito7 <guyhozanto@gmail.com>
2023-12-31 12:26:43 -0500

    Setting up plugins with Balls #15

--- home-manager/programs/neovim/nvim/lua/core/lazy.lua
+++ home-manager/programs/neovim/nvim/lua/core/lazy.lua
@@ -53,6 +53,21 @@ local plugins = {
 	["folke/tokyonight.nvim"] = {},
 	-- Oil
 	["stevearc/oil.nvim"] = {},
+	-- LSP + dependencies
+	["folke/neodev.nvim"] = {},
+	["neovim/nvim-lspconfig"] = {
+		event = { "BufReadPre", "BufNewFile" },
+	},
+	-- CMP + dependencies
+	["hrsh7th/cmp-path"] = {},
+	["hrsh7th/cmp-cmdline"] = {},
+	["L3MON4D3/LuaSnip"] = {},
+	["saadparwaiz1/cmp_luasnip"] = {},
+	["hrsh7th/cmp-nvim-lsp"] = {},
+	["rafamadriz/friendly-snippets"] = {},
+	["onsails/lspkind.nvim"] = {},
+	["hrsh7th/nvim-cmp"] = {},
+	
 }
 
 for url, opts in pairs(plugins) do
@@ -206,9 +221,9 @@ oil.setup({
 		default_file_explorer = true,
 		columns = {
 			"icon",
-			"permissions",
-			"size",
-			"mtime",
+			--"permissions",
+			--"size",
+			--"mtime",
 		},
 		buf_options = {
 			buflisted = false,
@@ -299,3 +314,221 @@ oil.setup({
 		},
 })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+-- LSP
+local lsp = vim.lsp
+
+local on_attach = function(_, bufnr)
+	local bufmap = function(keys, func)
+		vim.keymap.set("n", keys, func, { buffer = bufnr })
+	end
+
+	bufmap("<leader>r", vim.lsp.buf.rename)
+	bufmap("<leader>a", vim.lsp.buf.code_action)
+
+	bufmap("gd", vim.lsp.buf.definition)
+	bufmap("gD", vim.lsp.buf.declaration)
+	bufmap("gI", vim.lsp.buf.implementation)
+	bufmap("<leader>D", vim.lsp.buf.type_definition)
+
+	bufmap("gr", require("telescope.builtin").lsp_references)
+	bufmap("<leader>s", require("telescope.builtin").lsp_document_symbols)
+	bufmap("<leader>S", require("telescope.builtin").lsp_dynamic_workspace_symbols)
+
+	bufmap("<leader>h", vim.lsp.buf.hover)
+
+	vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+		vim.lsp.buf.format()
+	end, {})
+end
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+
+local util = require("lspconfig/util")
+
+require("neodev").setup()
+require("lspconfig").lua_ls.setup({
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				-- Get the language server to recognize the `vim` global
+				globals = { "vim" },
+			},
+			workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					vim.api.nvim_get_runtime_file("", true),
+					vim.fn.stdpath("config") .. "/lua",
+				},
+				checkThirdParty = false,
+			},
+		},
+	},
+})
+require("lspconfig").ruff_lsp.setup({
+	on_attach = on_attach,
+	init_options = {
+		settings = {
+			-- Any extra CLI arguments for `ruff` go here.
+			args = {},
+		},
+	},
+})
+require("lspconfig").jedi_language_server.setup({
+	init_options = {
+		codeAction = {
+			nameExtractVariable = "jls_extract_var",
+			nameExtractFunction = "jls_extract_def",
+		},
+		completion = {
+			disableSnippets = false,
+			resolveEagerly = false,
+			ignorePatterns = {},
+		},
+		diagnostics = {
+			enable = true,
+			didOpen = true,
+			didChange = true,
+			didSave = true,
+		},
+		hover = {
+			enable = true,
+			disable = {
+				class = { all = false, names = {}, fullNames = {} },
+				["function"] = { all = false, names = {}, fullNames = {} },
+				instance = { all = false, names = {}, fullNames = {} },
+				keyword = { all = false, names = {}, fullNames = {} },
+				module = { all = false, names = {}, fullNames = {} },
+				param = { all = false, names = {}, fullNames = {} },
+				path = { all = false, names = {}, fullNames = {} },
+				property = { all = false, names = {}, fullNames = {} },
+				statement = { all = false, names = {}, fullNames = {} },
+			},
+		},
+		jediSettings = {
+			autoImportModules = {},
+			caseInsensitiveCompletion = true,
+			debug = false,
+		},
+		markupKindPreferred = "markdown",
+		workspace = {
+			extraPaths = {},
+			symbols = {
+				ignoreFolders = { ".nox", ".tox", ".venv", "__pycache__", "venv" },
+				maxSymbols = 20,
+			},
+		},
+	},
+})
+
+require("lspconfig").rnix.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+
+require("lspconfig").rust_analyzer.setup({})
+require("lspconfig").clangd.setup({})
+require("lspconfig").nil_ls.setup({})
+require("lspconfig").gopls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+	cmd = { "gopls" },
+	filetypes = { "go", "gomod", "gowork", "gotmpl" },
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	settings = {
+		gopls = {
+			completeUnimported = true,
+			usePlaceholders = true,
+			analyses = {
+				unusedparams = true,
+			},
+		},
+	},
+})
+require("lspconfig").zls.setup({})
+
+lsp.handlers["textDocument/hover"] = lsp.with(vim.lsp.handlers.hover, {
+	border = "rounded",
+})
+
+-- Bordered LSP Diagnostics
+vim.diagnostic.config({
+	float = { border = "rounded" },
+})
+
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+local lspkind = require("lspkind")
+local max_items = 7
+
+-- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
+require("luasnip.loaders.from_vscode").lazy_load()
+
+cmp.setup({
+	dependencies = {
+		cmp_path,
+	},
+	completion = {
+		completeopt = "menu,menuone,preview,noselect",
+	},
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+	}),
+	sources = {
+		{ name = "nvim_lsp", max_item_count = max_items },
+		{ name = "luasnip", max_item_count = max_items },
+		{ name = "neorg", max_item_count = max_items },
+		{ name = "path", max_item_count = max_items },
+	},
+	window = {
+		completion = cmp.config.window.bordered({
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		}),
+		documentation = cmp.config.window.bordered({
+			border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+		}),
+		scrollbar = false,
+	},
+	-- configure lspkind for vs-code like pictograms in completion menu
+	formatting = {
+		format = lspkind.cmp_format({
+			mode = "symbol",
+			maxwidth = 50,
+			ellipsis_char = "...",
+		}),
+	},
+})
+
+cmp.setup.cmdline({ "/", "?" }, {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer", max_item_count = max_items },
+	},
+})
+
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path", max_item_count = max_items },
+	}, {
+		{ name = "cmdline", max_item_count = max_items },
+	}),
+})
+-- If you want insert `(` after select function or method item
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+local cmp_auto = require("cmp")
+cmp_auto.event:on("confirm_done", cmp_autopairs.on_confirm_done())
