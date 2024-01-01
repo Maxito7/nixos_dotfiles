39db8f45c708efab3baddb17462319a13ebe4a8e
Maxito7 <guyhozanto@gmail.com>
2023-12-31 20:02:24 -0500

    Properly added (hopefully) Ruff formatter

--- home-manager/programs/neovim/nvim/lua/core/balls.lua
+++ home-manager/programs/neovim/nvim/lua/core/balls.lua
@@ -899,7 +899,7 @@ require("conform").setup({
 	formatters_by_ft = {
 		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
-		python = { "ruff" },
+		python = { "ruff_format" },
 		go = { "gofumpt" },
 		-- Use a sub-list to run only the first available formatter
 		--javascript = { { "prettierd", "prettier" } },
