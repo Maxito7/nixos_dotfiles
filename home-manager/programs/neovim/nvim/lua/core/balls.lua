bc73b915a0906514823f0984b50039100662708d
Maxito7 <guyhozanto@gmail.com>
2024-01-02 18:39:58 -0500

    Attempt at devicons #5

--- home-manager/programs/neovim/nvim/lua/core/balls.lua
+++ home-manager/programs/neovim/nvim/lua/core/balls.lua
@@ -897,10 +897,17 @@ require("colorizer").setup({})
 -- Conform setup
 vim.cmd.packadd("conform.nvim")
 require("conform").setup({
+	formatters = {
+		ruff_isort = {
+			command = "ruff",
+			args = { "check", "--select", "I", "--fix", "--stdin-filename", "$FILENAME" },
+			stdin = true,
+		},
+	},
 	formatters_by_ft = {
 		lua = { "stylua" },
 		-- Conform will run multiple formatters sequentially
-		python = { "ruff_format" },
+		python = { "ruff_isort", "ruff_format" },
 		go = { "gofumpt" },
 		-- Use a sub-list to run only the first available formatter
 		--javascript = { { "prettierd", "prettier" } },
