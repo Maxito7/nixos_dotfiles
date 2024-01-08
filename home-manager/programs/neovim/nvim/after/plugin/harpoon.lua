f7f9e0f4f863bafb6adf54eb1c20c11b2270fd44
Maxito7 <guyhozanto@gmail.com>
2024-01-07 22:37:23 -0500

    Maybe some Harpoon?

--- home-manager/programs/neovim/nvim/after/plugin/harpoon.lua
+++ home-manager/programs/neovim/nvim/after/plugin/harpoon.lua
@@ -1,60 +1,22 @@
---[[
-vim.cmd.packadd("harpoon")
 local harpoon = require("harpoon")
-
--- REQUIRED
-harpoon:setup()
--- REQUIRED
-
-vim.keymap.set("n", "<leader>a", function()
+local extensions = require("harpoon.extensions")
+harpoon:setup({
+	--     settings = {
+	--         save_on_toggle = true,
+	--         sync_on_ui_close = true,
+	--     },
+})
+-- harpoon:extend(extensions.builtins.navigate_with_number())
+vim.keymap.set("n", "<leader>ha", function()
 	harpoon:list():append()
 end)
-vim.keymap.set("n", "<C-e>", function()
+vim.keymap.set("n", "<leader>hm", function()
 	harpoon.ui:toggle_quick_menu(harpoon:list())
 end)
-
-vim.keymap.set("n", "<C-h>", function()
-	harpoon:list():select(1)
-end)
-vim.keymap.set("n", "<C-t>", function()
-	harpoon:list():select(2)
-end)
-vim.keymap.set("n", "<C-n>", function()
-	harpoon:list():select(3)
-end)
-vim.keymap.set("n", "<C-s>", function()
-	harpoon:list():select(4)
-end)
-
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function()
+vim.keymap.set("n", "<C-p>", function()
 	harpoon:list():prev()
 end)
-vim.keymap.set("n", "<C-S-N>", function()
+vim.keymap.set("n", "<C-n>", function()
 	harpoon:list():next()
 end)
-
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
-
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
-end
-
-vim.keymap.set("n", "<C-e>", function()
-	toggle_telescope(harpoon:list())
-end, { desc = "Open harpoon window" })
-]]
