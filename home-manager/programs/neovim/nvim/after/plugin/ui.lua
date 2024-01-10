22536591e1c5599d4a6ca45ccd1bdba67b9655e2
Maxito7 <guyhozanto@gmail.com>
2024-01-09 23:56:03 -0500

    Little aesthetic change to Nvim

--- home-manager/programs/neovim/nvim/after/plugin/ui.lua
+++ home-manager/programs/neovim/nvim/after/plugin/ui.lua
@@ -181,8 +181,8 @@ vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", {
 })
 -- BORDER GROUPS AND COLORS
 vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#7AA2F7" })
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#5f74a1" })
-vim.api.nvim_set_hl(0, "LineNr", { fg = "#86a1db" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3b4261" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#3b4261" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#3b4261" })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingError", { bg = "", fg = "#f7768e" })
 vim.api.nvim_set_hl(0, "DiagnosticFloatingHint", { bg = "", fg = "#7bd88f" })
