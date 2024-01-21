03d43939a658b19df1e3dc6a8258b6b761b4f513
Maxito7 <guyhozanto@gmail.com>
2024-01-20 20:30:58 -0500

    Added transient prompts

--- home-manager/configs/shells/fish.nix
+++ home-manager/configs/shells/fish.nix
@@ -1,15 +1,20 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   programs.fish = {
     enable = true;
-    interactiveShellInit = ''
-    	set fish_greeting
-	# Alias
-        alias flake_update="cd ~/.config/nixfiles ; nix flake update ; sudo nixos-rebuild switch --flake .#lucky"
-        alias project_devenv="nix flake init --template github:cachix/devenv && direnv allow"
-				alias ls="eza -l --icons=always --hyperlink -a"
+    interactiveShellInit = lib.mkAfter ''
+          	set fish_greeting
+      	# Alias
+              alias flake_update="cd ~/.config/nixfiles ; nix flake update ; sudo nixos-rebuild switch --flake .#lucky"
+              alias project_devenv="nix flake init --template github:cachix/devenv && direnv allow"
+      				alias ls="eza -l --icons=always --hyperlink -a"
 
-        direnv hook fish | source
+      				function starship_transient_prompt_func
+      					starship module_character
+      				end
+      				enable_transience
+
+              direnv hook fish | source
     '';
   };
 }
