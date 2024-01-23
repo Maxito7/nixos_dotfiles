5c8497f0fe0c8dceb6983823b80ec4e59c0ade62
Maxito7 <guyhozanto@gmail.com>
2024-01-23 14:51:44 -0500

    Added Ghostty #4

--- home-manager/programs/ghostty.nix
+++ home-manager/programs/ghostty.nix
@@ -1,27 +1,62 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.custom.ghostty;
+  inherit (config.custom) terminal;
+in {
+  config = lib.mkIf cfg.enable {
+    # open ghostty from nemo
+    custom.terminal.fakeGnomeTerminal = lib.mkIf (terminal.package == pkgs.ghostty) (pkgs.writeShellApplication {
+      name = "gnome-terminal";
+      text = ''
+        shift
 
-    # NOTE: This will require your git SSH access to the repo.
-    #
-    # WARNING: Do NOT pin the `nixpkgs` input, as that will
-    # declare the cache useless. If you do, you will have
-    # to compile LLVM, Zig and Ghostty itself on your machine,
-    # which will take a very very long time.
-    ghostty = {
-      url = "git+ssh://git@github.com/mitchellh/ghostty";
-    };
-  };
+        TITLE="$(basename "$1")"
+        if [ -n "$TITLE" ]; then
+          ${terminal.exec} --title "$TITLE" "$@"
+        else
+          ${terminal.exec} "$@"
+        fi
+      '';
+    });
 
-  outputs = { nixpkgs, ghostty, ... }: {
-    nixosConfigurations.mysystem = nixpkgs.lib.nixosSystem {
-      modules = [
-        {
-          environment.systemPackages = [
-            ghostty.packages.x86_64-linux.default
-          ];
-        }
-      ];
+    home = {
+      packages = [pkgs.ghostty];
+      sessionVariables = {
+        GHOSTTY_RESOURCES_DIR = "${pkgs.ghostty}/share";
+      };
     };
+
+    xdg.configFile."ghostty/config".text =
+      lib.generators.toKeyValue {
+        mkKeyValue = lib.generators.mkKeyValueDefault {} " = ";
+        listsAsDuplicateKeys = true;
+      } {
+        # adjust-cell-height = 1;
+        background = "#000000";
+        background-opacity = terminal.opacity;
+        command =
+          if (config.custom.shell.interactive == "fish")
+          then "${lib.getExe pkgs.fish}"
+          else "${lib.getExe pkgs.bash}";
+        confirm-close-surface = false;
+        copy-on-select = true;
+        cursor-style = "bar";
+        font-family = terminal.font;
+        font-feature = "zero";
+        font-size = terminal.size;
+        font-style = "Medium";
+        minimum-contrast = 1.1;
+        # term = "xterm-kitty";
+        # theme = "catppuccin-mocha";
+        window-decoration = false;
+        window-padding-x = terminal.padding;
+        window-padding-y = terminal.padding;
+      };
+
+    wayland.windowManager.hyprland.settings.bind = ["$mod, q, exec, ${lib.getExe pkgs.ghostty}"];
   };
 }
