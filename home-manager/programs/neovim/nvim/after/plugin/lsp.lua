1f4d374149a6f6f1f6e05ab975f8cf9420dcaa5b
Maxito7 <guyhozanto@gmail.com>
2024-01-23 11:25:03 -0500

    Removed rnix from LSP

--- home-manager/programs/neovim/nvim/after/plugin/lsp.lua
+++ home-manager/programs/neovim/nvim/after/plugin/lsp.lua
@@ -117,10 +117,12 @@ require("lspconfig").jedi_language_server.setup({
 	},
 })
 
+--[[
 require("lspconfig").rnix.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
+]]
 
 require("lspconfig").rust_analyzer.setup({
 	--[[
