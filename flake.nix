0e6683e0dd07870f3528eb3976cf95fb68b0ad4e
Maxito7 <guyhozanto@gmail.com>
2024-01-30 08:45:14 -0500

    Changed package repo for flakes

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/a136def4f7256146b32e3625e53dcdf7464165f9";
     nixpkgs-f2k.url = "github:moni-dz/nixpkgs-f2k";
     nur.url = "github:nix-community/NUR";
     #nixvim.url = "github:nix-community/nixvim";
@@ -10,61 +10,61 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-		ghostty = {
+    ghostty = {
       url = "git+ssh://git@github.com/mitchellh/ghostty";
-    };		
+    };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    home-manager,
-		ghostty,
-    #nixvim,
-    ...
-  } @ inputs: {
-    /* 
-      homeConfigurations = 
+  outputs =
+    { self
+    , nixpkgs
+    , home-manager
+    , ghostty
+    , #nixvim,
+      ...
+    } @ inputs: {
+      /*
+      homeConfigurations =
         let
-          overlays = [
-            inputs.neovim-nightly-overlay.overlay
-          ];
+      overlays = [
+        inputs.neovim-nightly-overlay.overlay
+      ];
         in {
         #macbook-pro = inputs.home-manager.lib.homeManagerConfiguration {
-          configuration = { pkgs, ... }:
-            {
-              nixpkgs.overlays = overlays;
-            };
+      configuration = { pkgs, ... }:
+        {
+          nixpkgs.overlays = overlays;
+        };
         #};
       };
-    */
-    nixosConfigurations = {
-      lucky = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit self inputs;
-        };
-        modules = [
-          ./configuration.nix
-          
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              extraSpecialArgs = { inherit inputs; };
-              users.lucky = import ./home-manager/home.nix;
-            };
-          }
+      */
+      nixosConfigurations = {
+        lucky = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = {
+            inherit self inputs;
+          };
+          modules = [
+            ./configuration.nix
 
-					{
-          environment.systemPackages = [
-            ghostty.packages.x86_64-linux.default
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                extraSpecialArgs = { inherit inputs; };
+                users.lucky = import ./home-manager/home.nix;
+              };
+            }
+
+            {
+              environment.systemPackages = [
+                ghostty.packages.x86_64-linux.default
+              ];
+            }
           ];
-        }
-        ];
+        };
       };
+      formatter."x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
     };
-    formatter."x86_64-linux" = nixpkgs.legacyPackages."x86_64-linux".alejandra;
-  };
 }
