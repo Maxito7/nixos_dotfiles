bb57d0544ece0e8288c0b0abea05bb339b5773ba
Maxito7 <guyhozanto@gmail.com>
2024-02-03 12:32:16 -0500

    Edited some colors for Tokyonight (Nvim)

--- home-manager/programs/neovim/nvim/after/plugin/tokyonight.lua
+++ home-manager/programs/neovim/nvim/after/plugin/tokyonight.lua
@@ -6,6 +6,11 @@ tokyonight.setup({
 	on_colors = function(colors)
 		colors.green = "#62d196"
 		colors.comment = "#86a1db"
+		colors.gitSigns = {
+			add = "#7bd88f",
+			change = "#7dcfff",
+			delte = "#f7768e",
+		}
 	end,
 	styles = {
 		floats = "transparent",
