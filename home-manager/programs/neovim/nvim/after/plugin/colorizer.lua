613bef1c09feac4df7389f384e1ff734d8b855c4
Maxito7 <guyhozanto@gmail.com>
2024-02-12 15:34:47 -0500

    Updated nvim-colorizer config

--- home-manager/programs/neovim/nvim/after/plugin/colorizer.lua
+++ home-manager/programs/neovim/nvim/after/plugin/colorizer.lua
@@ -2,6 +2,12 @@ require("colorizer").setup({
 	filetypes = { "*" },
 	user_default_options = {
 		names = false,
+		css = true,
+		RRGGBBAA = true,
+		AARRGGBB = true,
+		css_fn = true,
+		tailwind = true,
+		always_update = true,
 	},
 	buftypes = {},
 })
