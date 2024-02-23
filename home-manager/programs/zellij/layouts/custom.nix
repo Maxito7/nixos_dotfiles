1b9b132b988e447af4ff99e9d35d150d8d72d8c9
Maxito7 <guyhozanto@gmail.com>
2024-02-23 14:39:08 -0500

    Added Zellij through home-manager #20

--- home-manager/programs/zellij/layouts/custom.nix
+++ home-manager/programs/zellij/layouts/custom.nix
@@ -12,7 +12,7 @@ let
   white = "#C0CAF5";
   brightWhite = "#FFFFFF";
 
-  magenta = "#bb9af7";
+  purple = "#bb9af7";
   cyan = "#7dcfff";
   orange = "#ff9e64";
 
@@ -30,7 +30,7 @@ in
       plugin location="file:${inputs.zjstatus.packages.${pkgs.system}.default}/bin/zjstatus.wasm" {
         format_left  "{mode}"
   			format_center "{tabs}"
-  			format_right "#[bg=${green},fg=${black},bold]{session}"
+  			format_right "#[bg=${cyan},fg=${black},bold] {session} "
         format_space ""
 
         border_enabled  "false"
@@ -41,8 +41,10 @@ in
         hide_frame_for_single_pane "true"
 
   			mode_normal "#[bg=${blue},fg=${black},bold] {name} "
-  			mode_locked "#[bg=${orange},fg=${black},bold] {name} "
+  			mode_locked "#[bg=${red},fg=${black},bold] {name} "
         mode_tab "#[bg=${green},fg=${black}] {name} "
+  			mode_pane "#[bg=${purple},fg=${black}] {name} "
+  			mode_session "#[bg=${orange},fg=${black}] {name} "
         mode_default_to_mode "normal"
 
         tab_normal   "#[fg=${blue}] {index} #[fg=${white}] {name} "
