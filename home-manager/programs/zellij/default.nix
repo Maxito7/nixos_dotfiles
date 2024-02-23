8a191a8402240fa5b4acbe2319132f44a99ddbd6
Maxito7 <guyhozanto@gmail.com>
2024-02-23 10:15:38 -0500

    Added Zellij through home-manager #3

--- home-manager/programs/zellij/default.nix
+++ home-manager/programs/zellij/default.nix
@@ -1,5 +1,6 @@
 { config
 , pkgs
+, lib
 , libs
 , ...
 }: {
@@ -8,29 +9,11 @@
   ];
   programs.zellij = {
     enable = true;
+    enableFishIntegration = true;
   };
-
-  # Copy Zellij config, until it is properly supported by HM
-  xdg.configFile."zellij/config.kdl".source = ../configs/zellij/config.kdl;
-  xdg.configFile."zellij/layouts/default.kdl".text = ''    layout {
-    		default_tab_template {
-    			children
-    			pane size=1 borderless=true {
-    				plugin location="file:${pkgs.zjstatus}/bin/zjstatus.wasm" {
-    					hide_frame_for_single_pane "true"
-
-    					format_left "{mode} #[fg=#89B4FA,bg=#181825,bold]{session}#[bg=#181825] {tabs}"
-    					format_space "#[bg="181825]"
-
-    					mode_normal "#[bg=#89B4FA] "
-    					mode_tmux "#[bg=#FFC387] "
-    					mode_default_to_mode "tmux"
-
-    					tab_normal "#[fg=#6C7086,bg=#181825] {index} {name} "
-    					tab_actove "#[fg=#9399B2,bg=#181825,bold,italic] {italic} {name} "
-    				}
-    			}
-    		}
-    	}
-  '';
+  xdg.configFile = {
+    "zellij/config.kdl".source = ./config.kdl;
+    "zellij/themes/default.kdl".source = ./themes/default.kdl;
+    "zellij/layouts/custom.kdl".text = import ./layouts/custom.nix { inherit pkgs lib; };
+  };
 }
