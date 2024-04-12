953aa8fd38d05f3abad561fc17bbc4ab4b82f1d1
Maxito7 <guyhozanto@gmail.com>
2024-04-11 23:00:59 -0500

    Added Typst #2

--- home-manager/programs/neovim/nvim/after/plugin/conform.lua
+++ home-manager/programs/neovim/nvim/after/plugin/conform.lua
@@ -13,6 +13,7 @@ require("conform").setup({
 		nix = { "alejandra", "nixpkgs_fmt" },
 		javascript = { "biome" },
 		typescript = { "biome" },
+		typst = { "typstyle" },
 	},
 	format_on_save = {
 		-- These options will be passed to conform.format()
