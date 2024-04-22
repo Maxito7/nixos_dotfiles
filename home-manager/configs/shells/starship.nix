afcf5adcd98ce86a8fd0b32444f1963c93ee94d4
Maxito7 <guyhozanto@gmail.com>
2024-04-22 16:42:25 -0500

    starship: restyled prompt #3

--- home-manager/configs/shells/starship.nix
+++ home-manager/configs/shells/starship.nix
@@ -5,7 +5,7 @@
     settings = {
       add_newline = true;
       format = ''
-        [ 󰠖 ](bold fg:#62d196)$directory$nodejs$rust$golang$php$c$python$kotlin$java$zig$nix_shell$git_branch$git_status
+        $directory$nodejs$rust$golang$php$c$python$kotlin$java$zig$nix_shell$git_branch$git_status
         $character
       '';
       fill = {
