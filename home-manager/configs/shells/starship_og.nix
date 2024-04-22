b16a66e08eced27dfaa7972d15f152ea95edff7e
Maxito7 <guyhozanto@gmail.com>
2024-04-22 16:36:17 -0500

    starship: restyled prompt

--- home-manager/configs/shells/starship_og.nix
+++ home-manager/configs/shells/starship_og.nix
@@ -5,7 +5,7 @@
     settings = {
       add_newline = true;
       format = ''
-        [ 󰠖 ](bold fg:#62d196)$directory[ ]$nodejs$rust$golang$php$c$python$kotlin$java$zig[ ]$nix_shell[ ]$git_branch[ ]$git_status
+        [ 󰠖 ](bg:#62d196 bold fg:#121619)[](bg:#5e99ff fg:#62d196)$directory[](fg:#5e99ff bg:#24283b)$nodejs$rust$golang$php$c$python$kotlin$java$zig[](fg:#24283b bg:#272d40)$nix_shell[](fg:#272d40 bg:#30364a)$git_branch[](fg:#30364a bg:#363c4f)$git_status[](fg:#363c4f)
         $character
       '';
       fill = {
@@ -13,7 +13,7 @@
       };
       directory = {
         format = "([( $path )]($style))";
-        style = "bold fg:#5e99ff";
+        style = "bg:#5e99ff bold fg:#1a1b26";
 
         truncation_length = 1;
         truncation_symbol = " /";
@@ -27,72 +27,72 @@
 
       git_branch = {
         symbol = "";
-        style = "bold fg:#5e99ff";
+        style = "bg:#30364a bold fg:#5e99ff";
         format = "([( $symbol $branch )]($style))";
       };
 
       git_status = {
-        style = "bold fg:#7aa2f7";
+        style = "bg:#363c4f bold fg:#7aa2f7";
         format = "([( $all_status$ahead_behind )]($style))";
       };
 
       nodejs = {
         symbol = "";
-        style = "bold fg:#62d196";
+        style = "bg:#24283b bold fg:#62d196";
         format = "([( $symbol ($version) )]($style))";
       };
 
       rust = {
         symbol = "";
-        style = "bold fg:#FF9e64";
+        style = "bg:#24283b bold fg:#FF9e64";
         format = "([( $symbol ($version) )]($style))";
       };
 
       golang = {
         symbol = "󰟓";
-        style = "bold fg:#7dcfff";
+        style = "bg:#24283b bold fg:#7dcfff";
         format = "([( $symbol ($version) )]($style))";
       };
 
       php = {
         symbol = "";
-        style = "bold fg:#769ff0";
+        style = "bg:#24283b bold fg:#769ff0";
         format = "([( $symbol ($version) )]($style))";
       };
 
       c = {
         symbol = "";
-        style = "bold fg:#BB8AF7";
+        style = "bg:#24283b bold fg:#BB8AF7";
         format = "([( $symbol ($version) )]($style))";
       };
 
       python = {
         symbol = "󰌠";
-        style = "bold fg:#6fdc8c";
+        style = "bg:#24283b bold fg:#6fdc8c";
         format = "([( $symbol ($version) )]($style))";
       };
 
       kotlin = {
         symbol = "";
-        style = "bold fg:#bb9af7";
+        style = "bg:#24283b bold fg:#bb9af7";
         format = "([( $symbol ($version) )]($style))";
       };
 
       java = {
         symbol = "";
-        style = "bold fg:#ff9e64";
+        style = "bg:#24283b bold fg:#ff9e64";
         format = "([( $symbol ($version) )]($style))";
       };
 
       zig = {
         symbol = "";
-        style = "bold fg:#ff9e64";
+        style = "bg:#24283b bold fg:#ff9e64";
         format = "([( $symbol ($version) )]($style))";
       };
 
       nix_shell = {
-        impure_msg = "[  Impure ](bold fg:#8a3ffc)";
-        pure_msg = "[  Pure ](bold fg:#33b1ff)";
+        impure_msg = "[  Impure ](bg:#272d40 bold fg:#8a3ffc)";
+        pure_msg = "[  Pure ](bg:#272d40 bold fg:#33b1ff)";
         format = "$state";
       };
     };
