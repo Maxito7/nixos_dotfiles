f7895d85abb47bd631777fa22cc3baef336df222
Maxito7 <guyhozanto@gmail.com>
2024-05-29 14:30:05 -0500

    pls ffs work rubylsp -1

--- home-manager/programs/neovim/nvim/after/plugin/lsp.lua
+++ home-manager/programs/neovim/nvim/after/plugin/lsp.lua
@@ -190,44 +190,6 @@ require("lspconfig").zls.setup({})
 require("lspconfig").gleam.setup({
 	cmd = { "glas", "--stdio" },
 })
-local configs = require("lspconfig.configs")
-
-if not configs.ruby_lsp then
-	local enabled_features = {
-		"documentHighlights",
-		"documentSymbols",
-		"foldingRanges",
-		"selectionRanges",
-		-- "semanticHighlighting",
-		"formatting",
-		"codeActions",
-	}
-
-	configs.ruby_lsp = {
-		default_config = {
-			cmd = { "bundle", "exec", "ruby-lsp" },
-			filetypes = { "ruby" },
-			root_dir = util.root_pattern("Gemfile", ".git"),
-			init_options = {
-				enabledFeatures = enabled_features,
-			},
-			settings = {},
-		},
-		commands = {
-			FormatRuby = {
-				function()
-					vim.lsp.buf.format({
-						name = "ruby_lsp",
-						async = true,
-					})
-				end,
-				description = "Format using ruby-lsp",
-			},
-		},
-	}
-end
-
-require("lspconfig").ruby_lsp.setup({ on_attach = on_attach, capabilities = capabilities })
 --[[
 require("lspconfig").erlangls.setup({})
 require("lspconfig").elixirls.setup({
