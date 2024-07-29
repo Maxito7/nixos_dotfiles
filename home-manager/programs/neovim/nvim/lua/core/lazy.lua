ccc60a4d17ab16d4b334c5095a62cba492e597fc
Maxito7 <guyhozanto@gmail.com>
2024-07-28 20:47:36 -0500

    added markdown.nvim

--- home-manager/programs/neovim/nvim/lua/core/lazy.lua
+++ home-manager/programs/neovim/nvim/lua/core/lazy.lua
@@ -179,4 +179,11 @@ require("lazy").setup({
 		"slint-ui/vim-slint",
 		ft = "slint",
 	},
+	{
+		"MeanderingProgrammer/markdown.nvim",
+		main = "render-markdown",
+		opts = {},
+		name = "render-markdown", -- Only needed if you have another plugin named markdown.nvim
+		dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" },
+	},
 })
