49522611c3034fd94b8a5e8c94c293ba9dfe81e6
Maxito7 <guyhozanto@gmail.com>
2024-08-02 16:19:54 -0500

    general config (02/08/24) #2

--- flake.nix
+++ flake.nix
@@ -12,9 +12,9 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     ghostty = {
-      url = "git+ssh://git@github.com/mitchellh/ghostty";
-      inputs.nixpkgs-stable.follows = "nixpkgs-unstable";
-      inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+      url = "git+ssh://git@github.com/ghostty-org/ghostty";
+      #inputs.nixpkgs-stable.follows = "nixpkgs-unstable";
+      #inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
     };
     wfetch = {
       url = "github:iynaix/wfetch";
