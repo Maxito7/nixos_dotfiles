88950b49ab9063d31b39cc646494782eace999e3
Maxito7 <guyhozanto@gmail.com>
2024-08-03 18:04:47 -0500

    hoping to fix Ghostty lol

--- flake.nix
+++ flake.nix
@@ -13,8 +13,8 @@
     };
     ghostty = {
       url = "git+ssh://git@github.com/ghostty-org/ghostty";
-      #inputs.nixpkgs-stable.follows = "nixpkgs-unstable";
-      #inputs.nixpkgs-unstable.follows = "nixpkgs-unstable";
+      nixpkgs-stable.follows = "nixpkgs-unstable";
+      nixpkgs-unstable.follows = "nixpkgs-unstable";
     };
     wfetch = {
       url = "github:iynaix/wfetch";
