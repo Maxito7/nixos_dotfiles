6848913d6fc55b8068e054b882b4a32613be1c58
Maxito7 <guyhozanto@gmail.com>
2024-08-12 17:19:22 -0500

    added yazi #4

--- home-manager/configs/shells/fish.nix
+++ home-manager/configs/shells/fish.nix
@@ -6,17 +6,26 @@
   programs.fish = {
     enable = true;
     interactiveShellInit = ''
-          set fish_greeting
-          function postexec_newline --on-event fish_postexec
-            echo ""
-          end
-          # Alias
-       alias flake_update="cd ~/.config/nixfiles ; nix flake update ; nh os switch --hostname lucky"
-          alias project_devenv="nix flake init --template github:cachix/devenv && direnv allow"
-          alias ls="eza -l --icons=always --hyperlink -a"
-      alias lst="eza -l --icons=always --hyperlink -a -T"
+           set fish_greeting
+           function postexec_newline --on-event fish_postexec
+             echo ""
+           end
+           # Alias
+        alias flake_update="cd ~/.config/nixfiles ; nix flake update ; nh os switch --hostname lucky"
+           alias project_devenv="nix flake init --template github:cachix/devenv && direnv allow"
+           alias ls="eza -l --icons=always --hyperlink -a"
+       alias lst="eza -l --icons=always --hyperlink -a -T"
 
-          direnv hook fish | source
+           direnv hook fish | source
+
+      function yy
+      	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+      	yazi $argv --cwd-file="$tmp"
+      	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+      		cd -- "$cwd"
+      	end
+      	rm -f -- "$tmp"
+      end
     '';
   };
 }
