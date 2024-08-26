7eb02247aee4854653032a739cc3a4158c31a6d5
Maxito7 <guyhozanto@gmail.com>
2024-08-25 20:45:35 -0500

    Added Yazi plugins #2

--- home-manager/programs/yazi/default.nix
+++ home-manager/programs/yazi/default.nix
@@ -12,8 +12,8 @@
   xdg.configFile = {
     "yazi/yazi.toml".source = ./yazi.toml;
     "yazi/init.lua".source = ./init.lua;
-    "yazi/plugins/glow.yazi".source = ./plugins/glow.yazi;
-    "yazi/plugins/starship.yazi".source = ./plugins/starship.yazi;
-    "yazi/plugins/full-border.yazi".source = ./plugins/full-border.yazi;
+    "yazi/plugins/glow.yazi/".source = ./plugins/glow.yazi;
+    "yazi/plugins/starship.yazi/".source = ./plugins/starship.yazi;
+    "yazi/plugins/full-border.yazi/".source = ./plugins/full-border.yazi;
   };
 }
