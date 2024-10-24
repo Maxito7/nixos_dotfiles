ec12d6908f370a5f459b010c02d772546fa25488
Maxito7 <max.jacinto@pucp.edu.pe>
2024-10-24 11:37:02 -0500

    fix #4

--- home-manager/programs/neovim/nvim/after/plugin/ui.lua
+++ home-manager/programs/neovim/nvim/after/plugin/ui.lua
@@ -241,40 +241,6 @@ vim.api.nvim_set_hl(0, "MiniIndentscopeSymbol", {
 	fg = vim.api.nvim_get_hl(0, { name = "Function" }).fg,
 })
 
----@type LazySpec
-require("yazi.nvim").setup({
-	event = "VeryLazy",
-	keys = {
-		-- 👇 in this section, choose your own keymappings!
-		{
-			"<leader>-",
-			"<cmd>Yazi<cr>",
-			desc = "Open yazi at the current file",
-		},
-		{
-			-- Open in the current working directory
-			"<leader>cw",
-			"<cmd>Yazi cwd<cr>",
-			desc = "Open the file manager in nvim's working directory",
-		},
-		{
-			-- NOTE: this requires a version of yazi that includes
-			-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-			"<c-up>",
-			"<cmd>Yazi toggle<cr>",
-			desc = "Resume the last yazi session",
-		},
-	},
-	---@type YaziConfig
-	opts = {
-		-- if you want to open yazi instead of netrw, see below for more info
-		open_for_directories = false,
-		keymaps = {
-			show_help = "<f1>",
-		},
-	},
-})
-
 -- BORDER GROUPS AND COLORS
 vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#5e99ff" })
 --vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#4d5358" })
