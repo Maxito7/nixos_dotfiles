d971bab6195f5ca373c72412dc3e813a0ab7735f
Maxito7 <max.jacinto@pucp.edu.pe>
2024-10-24 00:39:57 -0500

    undid the thingie lol

--- home-manager/programs/neovim/nvim/lua/core/lazy.lua
+++ home-manager/programs/neovim/nvim/lua/core/lazy.lua
@@ -295,20 +295,20 @@ require("lazy").setup({
 			-- 👇 in this section, choose your own keymappings!
 			{
 				"<leader>-",
-				"<cmd>yy<cr>",
+				"<cmd>Yazi<cr>",
 				desc = "Open yazi at the current file",
 			},
 			{
 				-- Open in the current working directory
 				"<leader>cw",
-				"<cmd>yy cwd<cr>",
+				"<cmd>Yazi cwd<cr>",
 				desc = "Open the file manager in nvim's working directory",
 			},
 			{
 				-- NOTE: this requires a version of yazi that includes
 				-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
 				"<c-up>",
-				"<cmd>yy toggle<cr>",
+				"<cmd>Yazi toggle<cr>",
 				desc = "Resume the last yazi session",
 			},
 		},
