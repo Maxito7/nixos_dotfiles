aa803aca27718d93b65b05b0e495af40de560ec3
Maxito7 <max.jacinto@pucp.edu.pe>
2025-01-13 16:14:17 -0500

    added volar to nvim

--- home-manager/programs/neovim/nvim/after/plugin/lsp.lua
+++ home-manager/programs/neovim/nvim/after/plugin/lsp.lua
@@ -215,11 +215,25 @@ require("lspconfig").tinymist.setup({
 -- Web-dev
 require("lspconfig").svelte.setup({})
 -- Volar
+--[[
 require("lspconfig").ts_ls.setup({
 	on_attach = function(client)
 		client.server_capabilities.documentFormattingProvider = nil
 	end,
 })
+]]
+require("lspconfig").volar.setup({
+	-- add filetypes for typescript, javascript and vue
+	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+	init_options = {
+		vue = {
+			-- disable hybrid mode
+			hybridMode = false,
+		},
+	},
+})
+-- you must remove ts_ls setup
+-- lspconfig.ts_ls.setup {}
 
 -- Haskell
 --[[
